79e09b50f8875ee408eb6fa00c5203f2a0e7ce47
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-06-21 06:38:39 -0700

    main_files

---
+++ main.c
@@ -0,0 +1,122 @@
+#include "monty.h"
+
+/**
+ * get_opcode - function that returns a function pointer to buildin function
+ * @opcode: string with the opcode
+ *
+ * Return: fucntion pointer to function
+ */
+void (*get_opcode(char *opcode))(stack_t **stack, unsigned int line_number)
+{
+	unsigned int i = 0;
+	instruction_t instructions[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
+
+	for (i = 0; instructions[i].opcode; i++)
+	{
+		if (strcmp(instructions[i].opcode, opcode) == 0)
+			break;
+	}
+	return (instructions[i].f);
+}
+
+/**
+ * init_vars - initializates tthe variables of a vars_t structure
+ * @vars: pointer to a vars_t estructure
+ *
+ * Return: nothing
+ */
+void init_vars(vars_t *vars)
+{
+	vars->line = NULL;
+	vars->len_line = 0;
+	vars->fp = NULL;
+	vars->opcode = NULL;
+	vars->op = NULL;
+	vars->stack = NULL;
+	vars->lines = 1;
+}
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of srings with the arguments
+ *
+ * Return: 0 on succes
+ */
+int main(int argc, char *argv[])
+{
+	vars_t vars;
+
+	e_vars.len_stack = 0;
+	e_vars.stack_mode = STACK;
+	init_vars(&vars);
+	on_exit(exit_handler, &vars);
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	vars.fp = fopen(argv[1], "r");
+	if (vars.fp == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	for (; getline(&(vars.line), &(vars.len_line), vars.fp) != -1; vars.lines++)
+	{
+		vars.opcode = strtok(vars.line, " \n\t\r");
+		e_vars.op_arg = strtok(NULL, " \n\t\r");
+		if (vars.opcode == NULL || vars.opcode[0] == '#')
+			continue;
+		vars.op = get_opcode(vars.opcode);
+		if (vars.op != NULL)
+			vars.op(&(vars.stack), vars.lines);
+		else
+		{
+			dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n",
+				vars.lines, vars.opcode);
+			exit(EXIT_FAILURE);
+		}
+		free(vars.line);
+		vars.line = NULL;
+	}
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * exit_handler - handles the exit with the function on exit
+ * @status: exit status (UNUSED)
+ * @arg: argument in this case a vars_t structure
+ *
+ * Return: nothing
+ */
+void exit_handler(int status, void *arg)
+{
+	vars_t *vars = (vars_t *) arg;
+
+	(void) status;
+	if (vars->line != NULL)
+		free(vars->line);
+	if (vars->fp != NULL)
+		fclose(vars->fp);
+	if (vars->stack != NULL)
+		free_stack(vars->stack);
+}
---
+++ monty.h
@@ -0,0 +1,114 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define STACK 0
+#define QUEUE 1
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct variables_s - structure with all the main's variables
+ * @line: buffer for getline function
+ * @len_line: length of buffer for getline function
+ * @fp: file pointer
+ * @opcode: name of opcode
+ * @op: function pointer to opcodes functions
+ * @stack: pointer to a double linked list
+ * @lines: number of lines
+ *
+ * Description: estructure that contains the
+ * variables of the main program
+ */
+typedef struct variables_s
+{
+	char *line;
+	size_t len_line;
+	FILE *fp;
+	char *opcode;
+	void (*op)(stack_t **stack, unsigned int line_number);
+	stack_t *stack;
+	unsigned int lines;
+} vars_t;
+
+/**
+ * struct extern_variables_s - estruct of external variables
+ * @op_arg: argument of the actual opcode in main program
+ * @len_stack: stack length
+ * @stack_mode: stack mode, it could be STACK(0) or QUEUE(1)
+ *
+ * Description: structure that contains all the external variables
+ * because it can be declared only one external variable, in this
+ * case a instance of this structure
+ */
+typedef struct extern_variables_s
+{
+	char *op_arg;
+	unsigned int len_stack;
+	int stack_mode;
+} extern_variables_t;
+
+extern_variables_t e_vars;
+
+void (*get_opcode(char *opcode))(stack_t **stack, unsigned int line_number);
+void init_vars(vars_t *vars);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+stack_t *add_snode(stack_t **head, const int n);
+stack_t *add_dnodeint(stack_t **head, const int n);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+void free_stack(stack_t *head);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+void _stack(stack_t **stack, unsigned int line_number);
+void _queue(stack_t **stack, unsigned int line_number);
+
+#endif /* _MONTY_H_ */
+
